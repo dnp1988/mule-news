17de3275111abe3d53b47d2cf89c38d378989e34
Miguel Oliva <moliva@mulesoft.com>
2013-08-06 19:53:07 -0700

    Renamed message-flows to news-flows

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,1 +1,9 @@
-CREATE SCHEMA mulenews;
+CREATE TABLE news (
+	newsid INT NOT NULL,
+	title VARCHAR(50) NOT NULL DEFAULT '',
+	content MEDIUMTEXT NOT NULL,
+	url VARCHAR(50),
+	category VARCHAR(30),
+	createdAt (
+	PRIMARY KEY (newsid)
+);
